89e42b6359629bb97ab76f41af290aa23e19bdb1
Rafał Miłecki <rafal.milecki@mail.com>
2023-09-02 23:51:27 +0800

    move xray config file viewer to mics menu

--- files/luci/misc.js
+++ files/luci/misc.js
@@ -5,7 +5,23 @@
 
 return view.extend({
 
-    render:function(){
+    load: function() {
+        return Promise.all([
+            L.resolveDefault(fs.stat('/var/etc/xjay/config.json'), null)
+        ]).then(function(stat) {
+            var conf = stat[0] ? stat[0].path : null;
+
+            if (conf == null)
+                return 'The config file does not exist!';
+
+            return fs.exec_direct('/bin/cat', [ conf ]).catch(function(err) {
+                ui.addNotification(null, E('p', {}, _('Unable to load config file: ' + err.message)));
+                return '';
+            });
+        });
+    },
+
+    render:function(conf_data){
         var m, s, o;
         m = new form.Map('xjay', _('Misc.'), _('Misc. settings for xjay.'));
 
@@ -40,6 +56,28 @@ return view.extend({
 
         o = s.taboption('path', form.Value, 'xray_path', _('Xray Executable Path'), 'Please check if it is right file location for xray bin, or this will not work!');
         o.datatype = 'directory';
+
+        // as blow is the xray config view
+        s.tab('config', _('Config'));
+
+        o = s.taboption("config", form.DummyValue, "_configview");
+        o.render = L.bind(function() {
+            var conf_lines = conf_data.trim().split(/\n/);
+
+            return E([], [
+                E('h2', {}, [ _('Xray Config File') ]),
+                E('div', { 'id': 'content_config' }, [
+                    E('textarea', {
+                        'id': 'xray_config',
+                        'style': 'font-size:14px',
+                        'style': 'width:100%',
+                        'readonly': 'readonly',
+                        'wrap': 'off',
+                        'rows': conf_lines.length + 1
+                    }, [ conf_lines.join('\n') ])
+                ])
+            ]);
+        }, o, this);
 
         return m.render();
     }
